289c5b2bf4484b9fcb8d3344f56395d23abf121b
christian-ostermayer-5A <133903713+christian-ostermayer-5A@users.noreply.github.com>
2025-02-17 18:25:59 -0300

    Update checks.py

--- data_and_inputs_checks/checks.py
+++ data_and_inputs_checks/checks.py
@@ -840,7 +840,10 @@ def alocate_not_found_keys(df_1, df_2, shared_key_columns, value_columns_2, refe
             merged_df = merged_df.merge(starting_df_keys_not_found_2, on=list(set(shared_key_columns)-set([reference_key_column])), how='left',suffixes=('_x', ''))
             for col in merged_df.columns.values:
                 if "_x" in col:
-                    merged_df.drop(col, axis=1, inplace=True)
+                    try:
+                        merged_df.drop(col, axis=1, inplace=True)
+                    except:
+                        raise DataFrameError("An error occurred with the following DataFrame:", df_keys_not_found_2.columns.values, starting_df_keys_not_found_2.columns.values, shared_key_columns)
 
             # Separate found and not found keys
             found_keys = merged_df[merged_df['_merge'] == 'both'].drop(columns=['_merge'])
