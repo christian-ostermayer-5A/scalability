b21d191c252362b1dfb9380b9bb2195364f09c22
christian-ostermayer-5A <133903713+christian-ostermayer-5A@users.noreply.github.com>
2025-02-18 08:42:05 -0300

    Update checks.py

--- data_and_inputs_checks/checks.py
+++ data_and_inputs_checks/checks.py
@@ -848,7 +848,8 @@ def alocate_not_found_keys(df_1, df_2, shared_key_columns, value_columns_2, refe
       # Separate found and not found keys
       found_keys = merged_df[merged_df['_merge'] == 'both'].drop(columns=['_merge'])
       not_found_keys = merged_df[merged_df['_merge'] == 'left_only'].drop(columns=['_merge'])
-      
+      print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+      print(found_keys)
 
       # Add found keys to df_2
       if len(found_keys) > 0:
